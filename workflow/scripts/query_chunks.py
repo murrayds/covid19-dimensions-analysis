4ab33f10717f83e9be6afc04142cbe3db19d9a39
Dakota Murray <Dakota.S.Murray@gmail.com>
2022-04-28 13:34:39 -0400

    update query chunk documentation

--- workflow/scripts/query_chunks.py
+++ workflow/scripts/query_chunks.py
@@ -20,11 +20,14 @@ AND ('COVID-19' in UNNEST(concepts.concept) or "new coronavirus" in UNNEST(conce
       OR REGEXP_CONTAINS(title.preferred, r"covid-19|new coronavirus|novel coronavirus|sars-cov-2|2019-ncov|hcov|severe acute respiratory syndrome coronavirus|coronavirus disease 2019"))
 """
 
+# Filters to COVID-19 concepts, this time using a differnet format
+# more apt for an unnested table structure of publication concepts
 covid_filter_concepts_bqsql = """
 AND (LOWER(c.concept) IN UNNEST(["covid-19", "new coronavirus", "novel coronavirus", "sars-cov-2", "2019-ncov", "hcov", "hcov-2019", "severe acute respiratory syndrome coronavirus 2", "coronavirus disease 2019"])
     OR REGEXP_CONTAINS(title.preferred, r"covid-19|new coronavirus|novel coronavirus|sars-cov-2|2019-ncov|hcov|severe acute respiratory syndrome coronavirus|coronavirus disease 2019"))
 """
 
+# Filters to Vaccine-related concepts
 vaccine_filter_bqsql = """
 AND ('vaccine' in UNNEST(concepts.concept) or 'vaccination' in UNNEST(concepts.concept) or 'mRNA' in UNNEST(concepts.concept)
          OR REGEXP_CONTAINS(title.preferred, r"vaccine|vaccination|mrna"))
